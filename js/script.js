1cf55817f8a8c77006b1d2fac00c6f887dd99482
Simon Angatia <simonnbuko@gmail.com>
2019-06-27 12:57:45 -0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//select all figcaption elements to hide
+//document.getElementsByTagName('figcaption').forEach(figcaption =>{
+//    figcaption.addEventListener('mouseover', function() {
+//        this.removeAttr("class");
+//    });
+//});
+//
